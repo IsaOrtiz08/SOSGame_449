a64b49d4c709d4536971fa80c30fae3e5b5ee004
IsaOrtiz08 <ortizisa1008@gmail.com>
2024-10-01 19:14:22 -0500

    New Repo Started, Game_logic.cpp ready for testing

--- SOSGame_449/Game_logic.cpp
+++ SOSGame_449/Game_logic.cpp
@@ -2,20 +2,69 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 namespace SOS {
     class GameLogic {
     private:
-        int board_size = 3;
-        std::string Board[3][3];
+        int board_size;
+        std::vector<std::vector<std::string>> Board;
         int player1 = 1, player2 = 2;
         int currentPlayer;
         const std::string s = "S";
         const std::string o = "O";
         int player1Score = 0, player2Score = 0;
+        int gameMode = 0; //gamemode 0: simple, gamemode 1: general
 
     public:
         bool GameDone = false;
+
+        void GetBoardSize() {
+            int size;
+            std::cin >> size;
+            std::string error_large_board_size = "Invalid board size. Cannot exceed board larger than 15x15. Please try again";
+            std::string error_small_board_size = "Invalid board size. Board must be larger than 2x2. Please try again.";
+            if (size > 15) {
+                std::cout << error_large_board_size << std::endl;
+                GetBoardSize();
+            }
+            else if (size < 3) {
+                std::cout << error_small_board_size << std::endl;
+                GetBoardSize();
+            }
+            else {
+                board_size = size;
+            }
+        }
+
+        std::vector<std::vector<std::string>> CreateBoard() {
+            std::vector<std::vector<std::string>> board(board_size, std::vector<std::string>(board_size, ""));
+            Board = board;
+            return Board;
+        }
+
+        bool IsBoardFull() {
+            for (int i = 0; i < board_size; i++) {
+                for (int j = 0; j < board_size; j++) {
+                    if (Board[i][j] == "") {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        void ChooseGameMode(int input) {
+            if (input == 0) {
+                gameMode = 0;
+            }
+            else if (input == 1) {
+                gameMode = 1;
+            }
+            else {
+
+            }
+        }
 
         void SetNextPlayer() {
             currentPlayer = (currentPlayer == 1) ? 2 : 1;
@@ -55,11 +104,11 @@ namespace SOS {
                 std::cout << "Invalid move. Try again.\n";
                 return false;
             }
-            if (Board[row][col] != " ") {
+            if (Board[row][col] != "") {
                 std::cout << "Cell already taken. Choose another.\n";
                 return false;
             }
-
+  
             // Place the letter on the board
             Board[row][col] = letter;
 
@@ -77,6 +126,58 @@ namespace SOS {
             // Switch players
             currentPlayer = (currentPlayer == 1) ? 2 : 1;
             return true;
+        }
+
+        bool IsGameEnded_Simple() {
+            std::string player1wins = "Player 1 Wins!";
+            std::string player2wins = "Player 2 Wins!";
+            std::string draw = "Draw!";
+
+            // draw if both players have the same score and an SOS has been made
+            if (player1Score == player2Score && player1Score >= 1 && player2Score >= 1) {
+                std::cout << draw << std::endl;
+                return true;
+            }
+            // player1 wins if they scored a point
+            if (player1Score >= 1) {
+                std::cout << player1wins << std::endl;
+                return true;
+            }
+            // player 2 wins if they scored a point
+            if (player2Score >= 1) {
+                std::cout << player2wins << std::endl;
+                return true;
+            }
+            // no points made, continue game
+            else
+                return false;
+        }
+
+        bool IsGameEnded_General() {
+            std::string playerwins = "You Win!";
+            std::string computerwins = "Computer Wins!";
+            std::string draw = "Draw!";
+            // check if every spot on the board is occupied
+            if (!IsBoardFull)
+                return false;
+            // draw if both players have the same score
+            if (player1Score == player2Score) {
+                std::cout << draw << std::endl;
+                return true;
+            }
+            // player1 wins if score is higher
+            if (player1Score >= player2Score) {
+                std::cout << playerwins << std::endl;
+                return true;
+            }
+            // computer wins if score is higher
+            if (player2Score >= player1Score) {
+                std::cout << computerwins << std::endl;
+                return true;
+            }
+            // no points made, continue game
+            else
+                return false;
         }
     };
 }
