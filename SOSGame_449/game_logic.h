0e483c81a32141e1faa50e26716fd5565b1d0966
IsaOrtiz08 <113536391+IsaOrtiz08@users.noreply.github.com>
2024-11-11 15:38:03 -0600

    Update game_logic.h

--- SOSGame_449/game_logic.h
+++ SOSGame_449/game_logic.h
@@ -1,69 +1,71 @@
 #pragma once
-#ifndef GAME_LOGIC_H
-#define GAME_LOGIC_H
+//#ifndef GAME_LOGIC_H
+//#define GAME_LOGIC_H
 #include <string>
 #include <vector>
 #include <iostream>
 
 namespace SOS {
 
-class GameLogic {
-protected:
-    int board_size;
-    int player1 = 1, player2 = 2; // player 2 in general is the computer
-    int currentPlayer = 1;
-    const std::string s = "S";
-    const std::string o = "O";
-    std::string p1curr_letter = "";
-    std::string p2curr_letter = "";
-    int player1Score = 0, player2Score = 0;   
-public:
-    std::vector<std::pair<int, int>> completedSOSPositions;
-    int gameMode; //gamemode 0: simple, gamemode 1: general
-    GameLogic(int size, int mode);
-    std::vector<std::vector<std::string>> Board;
-    bool GameDone = false;
-    int getPlayer1Score();
-    int getPlayer2Score();
-    std::string get_p1curr_letter();
-    std::string get_p2curr_letter();
-    void SetP1CurrLetter(std::string letter);
-    void SetP2CurrLetter(std::string letter);
-    int getCurrentPlayer();
-    bool GetBoardSize(int input);
-    std::vector<std::vector<std::string>> CreateBoard();
-    bool IsBoardFull();
-    void SetNextPlayer();
-    int CheckForSOS(int row, int col);
-    bool makeMove(int row, int col, const std::string& letter);
-    virtual bool IsGameEnded() = 0;
-    const std::vector<std::pair<int, int>>& getCompletedSOSPositions();
-    void clearCompletedSOS();
-};
+    class GameLogic {
+    protected:
+        int board_size;
+        int player1 = 1, player2 = 2;
+        int player1_mode = 0, player2_mode = 0; // mode determines human or computer player, 0 is human, 1 is computer
+        int currentPlayer = 1;
+        const std::string s = "S";
+        const std::string o = "O";
+        std::string p1curr_letter = "";
+        std::string p2curr_letter = "";
+        int player1Score = 0, player2Score = 0;        
+        int lastMoveRow;
+        int lastMoveCol;
+        int computer_row = NULL, computer_column = NULL;
 
-class SimpleGameLogic : public GameLogic {
-public:
-    SimpleGameLogic(int size) : GameLogic(size, 0) {}
+    public:
+        std::vector<std::pair<int, int>> completedSOSPositions;
+        int gameMode; //gamemode 0: simple, gamemode 1: general
+        GameLogic(int size, int mode, int p1_mode, int p2_mode);
+        std::vector<std::vector<std::string>> Board;
+        bool GameDone = false;
+        int getPlayer1Score();
+        int getPlayer2Score();
+        std::string get_p1curr_letter();
+        std::string get_p2curr_letter();
+        void SetP1CurrLetter(std::string letter);
+        void SetP2CurrLetter(std::string letter);
+        int getCurrentPlayer();
+        bool GetBoardSize(int input);
+        std::vector<std::vector<std::string>> CreateBoard();
+        bool IsBoardFull();
+        void SetNextPlayer();
+        int CheckForSOS(int row, int col);
+        bool makeMove(int row, int col, const std::string& letter);
+        bool makeMoveComputer();
+        virtual bool IsGameEnded() = 0;
+        const std::vector<std::pair<int, int>>& getCompletedSOSPositions();
+        void clearCompletedSOS();        
+        int getLastMoveCol() const;
+        int getLastMoveRow() const;
+        std::string getComputerLetter() const;
+        void SetP1Mode(int mode);
+        void SetP2Mode(int mode);
+        int GetP1Mode();
+        int GetP2Mode();
+    };
 
-    // Override specific methods if needed
-    bool IsGameEnded();
-};
+    class SimpleGameLogic : public GameLogic {
+    public:
+        SimpleGameLogic(int size, int p1_mode, int p2_mode) : GameLogic(size, 0, p1_mode, p2_mode) {}
+        bool IsGameEnded();
+    };
 
-class GeneralGameLogic : public GameLogic {
-public:
-    GeneralGameLogic(int size) : GameLogic(size, 1) {}
-
-    bool makeMoveComputer();
-    bool IsGameEnded();
-    int getLastMoveCol() const;
-    int getLastMoveRow() const;
-    std::string getComputerLetter() const;
-private:
-    int computer_row, computer_column;
-     int lastMoveRow;
-    int lastMoveCol;
-};
+    class GeneralGameLogic : public GameLogic {
+    public:
+        GeneralGameLogic(int size, int p1_mode, int p2_mode) : GameLogic(size, 1, p1_mode, p2_mode) {}
+        bool IsGameEnded();
+    };
 
 
 }
-#endif // GAME_LOGIC_H
+//#endif // GAME_LOGIC_H
